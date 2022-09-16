4dcb59f00bbf2ed8185a4d8b4b8efe91d0cd8a38
minusminus <minusminus@tlen.pl>
2022-09-16 17:21:37 +0200

    - initial version

---
+++ TestTabliczkiMnozenia/Code/MultiplicationTestCasesGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestTabliczkiMnozenia.Code
+{
+    /// <summary>
+    /// Random test cases generator.
+    /// </summary>
+    internal static class MultiplicationTestCasesGenerator
+    {
+        public static List<TestItem> Generate(int count, int minNumberValue, int maxNumberValue)
+        {
+            Random random = new();
+
+            return Enumerable
+                .Range(1, count)
+                .Select(_ => CreateTestItem(random, minNumberValue, maxNumberValue))
+                .ToList();
+        }
+
+        private static TestItem CreateTestItem(Random random, int minNumberValue, int maxNumberValue)
+        {
+            int number1 = random.Next(minNumberValue, maxNumberValue + 1);
+            int number2 = random.Next(minNumberValue, maxNumberValue + 1);
+            return new TestItem(number1, number2, number1 * number2);
+        }
+    }
+}
---
+++ TestTabliczkiMnozenia/Code/UIElementsVisibility.cs
@@ -0,0 +1,19 @@
+﻿namespace TestTabliczkiMnozenia.Code
+{
+    /// <summary>
+    /// UI elements visibility.
+    /// </summary>
+    internal class UIElementsVisibility
+    {
+        public bool StartVisible = true;
+        public bool TestVisible = false;
+        public bool ResultVisible = false;
+
+        public void Set(bool startVisible, bool testVisible, bool resultVisible)
+        {
+            StartVisible = startVisible;
+            TestVisible = testVisible;
+            ResultVisible = resultVisible;
+        }
+    }
+}
